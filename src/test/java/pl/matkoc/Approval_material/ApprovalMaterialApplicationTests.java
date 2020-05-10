2fc984a1e0831bafd8257b7d56a87138e7d0b0a6
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-05-10 15:09:55 +0200

    Klasa startowa

---
+++ src/test/java/pl/matkoc/Approval_material/ApprovalMaterialApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matkoc.Approval_material;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprovalMaterialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
