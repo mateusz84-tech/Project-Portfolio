3fb9f075f1591a7dbd601974d3e13eeae4a66d44
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-05-10 15:17:33 +0200

    Konfiguracja pliku pom.xml o zależności dla mysql.

---
+++ src/main/java/pl/matkoc/Approval_material/ApprovalMaterialApplication.java
@@ -0,0 +1,13 @@
+package pl.matkoc.Approval_material;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalMaterialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprovalMaterialApplication.class, args);
+	}
+
+}
